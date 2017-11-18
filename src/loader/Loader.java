f8e298ac2f4da28c9ed862bf85de771d4ef4d58d
IvanHomonets <ivangomonets@ukr.net>
2017-11-18 21:55:48 +0300

    add code to class  loader from Stack

--- src/loader/Loader.java
+++ src/loader/Loader.java
@@ -1,4 +1,34 @@
 package loader;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 public class Loader {
+    public static void readFromWeb(String webURL) throws IOException {
+        URL url = new URL(webURL);
+        InputStream is =  url.openStream();
+        try( BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new MalformedURLException("URL is malformed!!");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new IOException();
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        String url = "https://github.com/open-numbers/ddf--gapminder--gdp_per_capita_cppp/blob/master/ddf--concepts.csv";
+        readFromWeb(url);
+    }
+
 }
